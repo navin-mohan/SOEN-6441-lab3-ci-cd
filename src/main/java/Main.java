51e4d400c74ac6ca89a3cf87e37982370f37bc6e
Navin Mohan <navinmohan81@gmail.com>
2025-04-03 15:51:12 -0400

    add custom class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,22 @@
+
+class Course {
+    String name;
+    int credits;
+
+    public Course(String name, int credits) {
+        this.name = name;
+        this.credits = credits;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", credits=" + credits +
+                '}';
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         IntList intList = new IntList();
@@ -31,5 +50,14 @@ public class Main {
         genericStringList.append("Orange");
         System.out.println(genericStringList);
 
+        GenericList<Course> courseList = new GenericList<>();
+        System.out.println(courseList);
+        courseList.append(new Course("SOEN6441", 4));
+        courseList.append(new Course("COMP6321", 4));
+        courseList.append(new Course("COEN6560", 4));
+        System.out.println(courseList);
+        courseList.delete();
+        courseList.append(new Course("INSE6760", 4));
+        System.out.println(courseList);
     }
 }
